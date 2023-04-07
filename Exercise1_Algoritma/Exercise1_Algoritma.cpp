c8443e085ecd6b210236e4e0dcc72c1248cdb032
MuhSaktiR <seken876@gmail.com>
2023-04-07 10:47:13 +0700

    untuk menampilkan cout Masukkan jumlah data maksimum dan input data cin

--- Exercise1_Algoritma/Exercise1_Algoritma.cpp
+++ Exercise1_Algoritma/Exercise1_Algoritma.cpp
@@ -6,5 +6,6 @@ const string INISIAL = "SR"; // Untuk nanti mengganti "j" menjadi 2 inisial SR
 const string NICKNAME = "Sakti"; // Untuk mengganti "arr" menjadi nama panggilan Sakti
 
 void input(int a[], int& n) {
-
+	cout << "Masukkan jumlah data (maksimum " << MAKS_DATA << "): ";
+	cin >> n;
 }
