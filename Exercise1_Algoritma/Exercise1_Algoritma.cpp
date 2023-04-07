d54b7d48fcd91d1b0ed03bf8083750f003afd0b6
MuhSaktiR <seken876@gmail.com>
2023-04-07 10:44:20 +0700

    mendeklarasi string inisial SR sebagai Sakti Ramadhan

--- Exercise1_Algoritma/Exercise1_Algoritma.cpp
+++ Exercise1_Algoritma/Exercise1_Algoritma.cpp
@@ -2,3 +2,4 @@
 using namespace std;
 
 const int MAKS_DATA = 109; // maksimum data dari 2 digit nim akhir (89) + 20 - 2 * 15 + 10 + 20
+const string INISIAL = "SR"; // Untuk nanti mengganti "j" menjadi 2 inisial SR
