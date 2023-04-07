de60414803be46e9c9d89188c346db480857c2f2
MuhSaktiR <seken876@gmail.com>
2023-04-07 10:46:32 +0700

    prosedur input untuk memasukkan data

--- Exercise1_Algoritma/Exercise1_Algoritma.cpp
+++ Exercise1_Algoritma/Exercise1_Algoritma.cpp
@@ -4,3 +4,7 @@ using namespace std;
 const int MAKS_DATA = 109; // maksimum data dari 2 digit nim akhir (89) + 20 - 2 * 15 + 10 + 20
 const string INISIAL = "SR"; // Untuk nanti mengganti "j" menjadi 2 inisial SR
 const string NICKNAME = "Sakti"; // Untuk mengganti "arr" menjadi nama panggilan Sakti
+
+void input(int a[], int& n) {
+
+}
